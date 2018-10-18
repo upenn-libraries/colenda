278e6afe261c97bead86635ba3fcec61836c6e2a
Katherine Lynch <katherly@upenn.edu>
2018-10-18 09:39:13 -0400

    Modifying rows

--- app/assets/javascripts/colenda.js
+++ app/assets/javascripts/colenda.js
@@ -1,4 +1,24 @@
-renderViewer = function() {
+renderSequenceViewer = function() {
+
+    imagesArray = jQuery.parseJSON($("#pages").attr("data"));
+    assetsHash = jQuery.parseJSON($("#navImages").attr("data"));
+
+    var viewer = OpenSeadragon({
+        id: "openseadragon",
+        prefixUrl: '',
+        showNavigator:  true,
+        navigatorPosition:   "TOP_RIGHT",
+        sequenceMode:       true,
+        showReferenceStrip: false,
+        collectionMode:       false,
+        showRotationControl: true,
+        tileSources: imagesArray,
+        navImages: assetsHash
+    });
+    return viewer.bookmarkUrl();
+}
+
+renderCollectionViewer = function() {
 
     imagesArray = jQuery.parseJSON($("#pages").attr("data"));
     assetsHash = jQuery.parseJSON($("#navImages").attr("data"));
@@ -20,8 +40,6 @@ renderViewer = function() {
         tileSources: imagesArray,
         navImages: assetsHash
     });
-
-
     return viewer.bookmarkUrl();
 }
 
